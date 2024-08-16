18e88f13ff51d2ad0c2e428a72b4b4a1bdf3e944
Marco Hurtado <m.hurtado@evolucare.com>
2024-08-16 13:01:51 +0200

    add home component

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+
+@NgModule({
+  imports: [],
+  declarations: [
+    AppComponent,
+    HomeComponent,   
+],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
